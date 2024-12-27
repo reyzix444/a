e34821dd51f8a773c38109131d6932e6dce8aa2c
reyzix444 <149589918+reyzix444@users.noreply.github.com>
2024-12-27 02:16:10 +0100

    cr.php

--- cr.php
+++ cr.php
@@ -1,1 +1,1 @@
-{"status":"heheheboy","key":"KYOJIN LKING","amount":"99999999999 KYOJIN","UUID":"2DE30CF2-D125-4E60-8735-FC05852A89B0","time":"2024-12-26 23:26:03","ip":"2a02:aee3f:aaaa:c101:2993:rr3c:dc87:0000, 000.000.000.000","agent":"Free%20Fire\/2019117860 CFNetwork\/1399 Darwin\/22.1.0"}
+{"status":"heheheboy","key":"REYZIX LKING","amount":"99999999999 REYZIX","UUID":"2DE30CF2-D125-4E60-8735-FC05852A89B0","time":"2024-12-26 23:26:03","ip":"2a02:aee3f:aaaa:c101:2993:rr3c:dc87:0000, 000.000.000.000","agent":"Free%20Fire\/2019117860 CFNetwork\/1399 Darwin\/22.1.0"}
